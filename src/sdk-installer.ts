b39f1a9f8b0a23a4fb53f370aa75a0b6cd6051eb
Yang <ychescale9@gmail.com>
2020-06-20 17:45:02 +1000

    Support running multiple actions sequentially in a single job.

--- src/sdk-installer.ts
+++ src/sdk-installer.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import * as fs from 'fs';
 
 const BUILD_TOOLS_VERSION = '30.0.0';
 const CMDLINE_TOOLS_URL_MAC = 'https://dl.google.com/android/repository/commandlinetools-mac-6514223_latest.zip';
@@ -11,24 +13,34 @@ const CMDLINE_TOOLS_URL_LINUX = 'https://dl.google.com/android/repository/comman
  */
 export async function installAndroidSdk(apiLevel: number, target: string, arch: string, emulatorBuild?: string, ndkVersion?: string, cmakeVersion?: string): Promise<void> {
   const isOnMac = process.platform === 'darwin';
-  console.log('Installing new cmdline-tools.');
-  const sdkUrl = isOnMac ? CMDLINE_TOOLS_URL_MAC : CMDLINE_TOOLS_URL_LINUX;
-  await exec.exec(`sudo mkdir ${process.env.ANDROID_HOME}/cmdline-tools`);
-  await exec.exec(`curl -fo commandlinetools.zip ${sdkUrl}`);
-  await exec.exec(`sudo unzip -q commandlinetools.zip -d ${process.env.ANDROID_HOME}/cmdline-tools`);
-  await exec.exec(`sudo rm -f commandlinetools.zip`);
 
-  // add paths for commandline-tools and platform-tools
-  core.addPath(`${process.env.ANDROID_HOME}/cmdline-tools/tools:${process.env.ANDROID_HOME}/cmdline-tools/tools/bin:${process.env.ANDROID_HOME}/platform-tools`);
-
-  // additional permission and license requirements for Linux
   if (!isOnMac) {
     await exec.exec(`sh -c \\"sudo chown $USER:$USER ${process.env.ANDROID_HOME} -R`);
-    await exec.exec(`sh -c \\"echo -e '\n84831b9409646a918e30573bab4c9c91346d8abd' > ${process.env.ANDROID_HOME}/licenses/android-sdk-preview-license"`);
   }
+
+  const cmdlineToolsPath = `${process.env.ANDROID_HOME}/cmdline-tools`;
+  if (!fs.existsSync(cmdlineToolsPath)) {
+    console.log('Installing new cmdline-tools.');
+    const sdkUrl = isOnMac ? CMDLINE_TOOLS_URL_MAC : CMDLINE_TOOLS_URL_LINUX;
+    await io.mkdirP(`${process.env.ANDROID_HOME}/cmdline-tools`);
+    await exec.exec(`curl -fo commandlinetools.zip ${sdkUrl}`);
+    await exec.exec(`unzip -q commandlinetools.zip -d ${cmdlineToolsPath}`);
+    await io.rmRF('commandlinetools.zip');
+
+    // add paths for commandline-tools and platform-tools
+    core.addPath(`${cmdlineToolsPath}/tools:${cmdlineToolsPath}/tools/bin:${process.env.ANDROID_HOME}/platform-tools`);
+  }
+
+  // additional permission and license requirements for Linux
+  const sdkPreviewLicensePath = `${process.env.ANDROID_HOME}/licenses/android-sdk-preview-license`;
+  if (!isOnMac && !fs.existsSync(sdkPreviewLicensePath)) {
+    await exec.exec(`sh -c \\"echo -e '\n84831b9409646a918e30573bab4c9c91346d8abd' > ${sdkPreviewLicensePath}"`);
+  }
+
   // license required for API 30 system images
-  if (apiLevel == 30) {
-    await exec.exec(`sh -c \\"echo -e '\n859f317696f67ef3d7f30a50a5560e7834b43903' > ${process.env.ANDROID_HOME}/licenses/android-sdk-arm-dbt-license"`);
+  const sdkArmDbtLicensePath = `${process.env.ANDROID_HOME}/licenses/android-sdk-arm-dbt-license`;
+  if (apiLevel == 30 && !fs.existsSync(sdkArmDbtLicensePath)) {
+    await exec.exec(`sh -c \\"echo -e '\n859f317696f67ef3d7f30a50a5560e7834b43903' > ${sdkArmDbtLicensePath}"`);
   }
 
   console.log('Installing latest build tools, platform tools, and platform.');
@@ -37,9 +49,9 @@ export async function installAndroidSdk(apiLevel: number, target: string, arch: 
   if (emulatorBuild) {
     console.log(`Installing emulator build ${emulatorBuild}.`);
     await exec.exec(`curl -fo emulator.zip https://dl.google.com/android/repository/emulator-${isOnMac ? 'darwin' : 'linux'}-${emulatorBuild}.zip`);
-    await exec.exec(`sudo rm -rf ${process.env.ANDROID_HOME}/emulator`);
-    await exec.exec(`sudo unzip -q emulator.zip -d ${process.env.ANDROID_HOME}`);
-    await exec.exec(`sudo rm -f emulator.zip`);
+    await io.rmRF(`${process.env.ANDROID_HOME}/emulator`);
+    await exec.exec(`unzip -q emulator.zip -d ${process.env.ANDROID_HOME}`);
+    await io.rmRF('emulator.zip');
   } else {
     console.log('Installing latest emulator.');
     await exec.exec(`sh -c \\"sdkmanager --install emulator > /dev/null"`);
