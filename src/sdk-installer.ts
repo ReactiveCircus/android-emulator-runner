fbe3cc33d97e3e9fac1c96dd8f6d9a3664d2d9a6
Yang <reactivecircus@gmail.com>
2021-07-21 23:19:53 +1000

    Update build-tools to 31.0.0. Add API 31 emulator to workflow. Update SDK command-line tools to 5.0

--- src/sdk-installer.ts
+++ src/sdk-installer.ts
@@ -4,9 +4,9 @@ import * as io from '@actions/io';
 import * as tc from '@actions/tool-cache';
 import * as fs from 'fs';
 
-const BUILD_TOOLS_VERSION = '30.0.3';
-const CMDLINE_TOOLS_URL_MAC = 'https://dl.google.com/android/repository/commandlinetools-mac-6858069_latest.zip';
-const CMDLINE_TOOLS_URL_LINUX = 'https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip';
+const BUILD_TOOLS_VERSION = '31.0.0';
+const CMDLINE_TOOLS_URL_MAC = 'https://dl.google.com/android/repository/commandlinetools-mac-7583922_latest.zip';
+const CMDLINE_TOOLS_URL_LINUX = 'https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip';
 
 /**
  * Installs & updates the Android SDK for the macOS platform, including SDK platform for the chosen API level, latest build tools, platform tools, Android Emulator,
