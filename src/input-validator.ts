2b4826e4e8cfb35eab5ce84c646d51dbb419b7c3
Alex Vanyo <vanyo@google.com>
2023-02-08 18:29:44 -0600

    Add android-desktop target option

--- src/input-validator.ts
+++ src/input-validator.ts
@@ -1,5 +1,16 @@
 export const MIN_API_LEVEL = 15;
-export const VALID_TARGETS: Array<string> = ['default', 'google_apis', 'aosp_atd', 'google_atd', 'google_apis_playstore', 'android-wear', 'android-wear-cn', 'android-tv', 'google-tv'];
+export const VALID_TARGETS: Array<string> = [
+  'default',
+  'google_apis',
+  'aosp_atd',
+  'google_atd',
+  'google_apis_playstore',
+  'android-wear',
+  'android-wear-cn',
+  'android-tv',
+  'google-tv',
+  'android-desktop',
+];
 export const VALID_ARCHS: Array<string> = ['x86', 'x86_64', 'arm64-v8a'];
 export const VALID_CHANNELS: Array<string> = ['stable', 'beta', 'dev', 'canary'];
 
