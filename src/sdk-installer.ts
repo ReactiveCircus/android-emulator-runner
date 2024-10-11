7ff545cc8917d3f6898d849b0ffed6fafb2dc7e1
Yang <ychescale9@gmail.com>
2024-10-11 11:02:55 +1100

    update cmdline-tools path.

--- src/sdk-installer.ts
+++ src/sdk-installer.ts
@@ -29,7 +29,7 @@ export async function installAndroidSdk(apiLevel: string, target: string, arch: 
     }
 
     // add paths for commandline-tools and platform-tools
-    core.addPath(`${cmdlineToolsPath}/latest:${cmdlineToolsPath}/latest/bin:${process.env.ANDROID_HOME}/platform-tools`);
+    core.addPath(`${cmdlineToolsPath}/12.0:${cmdlineToolsPath}/12.0/bin:${process.env.ANDROID_HOME}/platform-tools`);
 
     // set standard AVD path
     core.exportVariable('ANDROID_AVD_HOME', `${process.env.HOME}/.android/avd`);
